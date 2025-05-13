7ea5012479204f84fce2cd8d79ec33722009487a
marlon <marlonsuyuc@gmail.com>
2025-05-12 18:58:46 -0600

    (web) feat: adds diferents types of  buttons

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,18 +2,66 @@
 
 import { toast } from "ms-ui-toast";
 
+export type TypeToast = "SUCCESS" | "ERROR" | "WARNING" | "INFO";
+
 export default function Home() {
-  const handleClick = () => {
+  const handleClick = (type: TypeToast, title: string, description: string) => {
     toast({
-      title: "oh! Something went wrong",
-      description: "there was a problem with your request",
-      options: { type: "SUCCESS", duration: 3000 },
+      title,
+      description,
+      options: { type },
     });
   };
 
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <button onClick={handleClick}>Mostrar Toast</button>
+    <div className="bg-slate-900 w-full h-screen flex flex-col [&>button]:bg-white/90 [&>button]:w-48 [&>button]:p-4 justify-center items-center gap-5">
+      <button
+        onClick={() =>
+          handleClick(
+            "SUCCESS",
+            "Done",
+            "Your request was completed successfully"
+          )
+        }
+      >
+        SUCCESS
+      </button>
+
+      <button
+        onClick={() =>
+          handleClick(
+            "WARNING",
+            "Heads up!",
+            "This action may have unexpected effects"
+          )
+        }
+      >
+        WARNING
+      </button>
+
+      <button
+        onClick={() =>
+          handleClick(
+            "ERROR",
+            "oh! Something went wrong",
+            "there was a problem with your request"
+          )
+        }
+      >
+        ERROR
+      </button>
+
+      <button
+        onClick={() =>
+          handleClick(
+            "INFO",
+            "Just so you know",
+            "Here’s some useful information for you"
+          )
+        }
+      >
+        INFO
+      </button>
     </div>
   );
 }

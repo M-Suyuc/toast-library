fa9e5412f3f0e6720d067b6c522c751df6aa1f40
marlon <marlonsuyuc@gmail.com>
2025-05-15 18:10:45 -0600

    (library) feat: integrate Toast component and update button click handlers for toast notifications

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { toast } from "ms-ui-toast";
+import Toast from "../../../toast/src/Toast";
 
 export type TypeToast = "SUCCESS" | "ERROR" | "WARNING" | "INFO";
 
@@ -15,13 +16,19 @@ export default function Home() {
 
   return (
     <div className="bg-slate-900 w-full h-screen flex flex-col [&>button]:bg-white/90 [&>button]:w-48 [&>button]:p-4 justify-center items-center gap-5">
+      <Toast
+        title={"hello"}
+        description={"Hi over there Hi over there"}
+        position={"top-right"}
+        id={1}
+      />
+
       <button
         onClick={() =>
-          handleClick(
-            "SUCCESS",
-            "Done",
-            "Your request was completed successfully"
-          )
+          toast({
+            title: "Done",
+            description: "Your request was successfully",
+          })
         }
       >
         SUCCESS
@@ -29,11 +36,7 @@ export default function Home() {
 
       <button
         onClick={() =>
-          handleClick(
-            "WARNING",
-            "Heads up!",
-            "This action may have unexpected effects"
-          )
+          handleClick("WARNING", "Heads up!", "This action may have effects")
         }
       >
         WARNING
@@ -44,7 +47,7 @@ export default function Home() {
           handleClick(
             "ERROR",
             "oh! Something went wrong",
-            "there was a problem with your request"
+            "there was a problem "
           )
         }
       >
@@ -56,7 +59,7 @@ export default function Home() {
           handleClick(
             "INFO",
             "Just so you know",
-            "Here’s some useful information for you"
+            "Here’s some useful information"
           )
         }
       >

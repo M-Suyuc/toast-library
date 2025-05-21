7550aba23e352ddf46b4273c4e89ed409dd3f20e
marlon <marlonsuyuc@gmail.com>
2025-05-20 19:16:16 -0600

    (library) feat: implement toast functions for handling different toast variants

---
+++ toast/src/toast-functions.ts
@@ -0,0 +1,30 @@
+import { ShowToast } from "./ToastContext";
+
+import type { ToastFunctions, ToastWithVariant } from "./types/toast.types";
+
+export const toast: ToastFunctions = {
+  default(data: ToastWithVariant) {
+    ShowToast({ ...data, variant: "DEFAULT" });
+    return data;
+  },
+
+  success: (data) => {
+    ShowToast({ ...data, variant: "SUCCESS" });
+    return data;
+  },
+
+  info: (data) => {
+    ShowToast({ ...data, variant: "INFO" });
+    return data;
+  },
+
+  error: (data) => {
+    ShowToast({ ...data, variant: "ERROR" });
+    return data;
+  },
+
+  warning: (data) => {
+    ShowToast({ ...data, variant: "WARNING" });
+    return data;
+  },
+};

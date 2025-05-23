84dae8f7a62e2bf0afb056161fc949acd7884d28
marlon <marlonsuyuc@gmail.com>
2025-05-22 18:05:19 -0600

    (library) refactor: simplify Toast component structure and enhance animation handling

--- toast/src/Toast.tsx
+++ toast/src/Toast.tsx
@@ -1,106 +1,43 @@
 "use client";
 
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { ToastWithVariant } from "./types/toast.types";
+
+import { Error, Info, Success, Warning } from "./icons";
 
 import "./index.css";
 
 const Toast: React.FC<ToastWithVariant> = ({
   title,
   description,
-  position = "bottom-right",
-  id,
-  variant,
+  variant = "DEFAULT",
 }) => {
-  const toastClass = `toast toast--${variant?.toLowerCase()} ${position}`;
+  const toastClass = `toast toast--${variant?.toLowerCase()}  `;
 
   return (
-    <motion.div
-      animate={{ y: 0, opacity: 1 }}
-      exit={{ opacity: 0, scale: 0.5 }}
-      className={`${toastClass}`}
-      style={{
-        bottom: `${id * 80}px`,
-      }}
-    >
-      <span>
-        {variant === "SUCCESS" ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-            <path d="M9 12l2 2l4 -4" />
-          </svg>
-        ) : variant === "ERROR" ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-            <path d="M12 9v4" />
-            <path d="M12 16v.01" />
-          </svg>
-        ) : variant === "WARNING" ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 9v4" />
-            <path d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z" />
-            <path d="M12 16h.01" />
-          </svg>
-        ) : variant === "INFO" ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-            <path d="M12 9h.01" />
-            <path d="M11 12h1v4h1" />
-          </svg>
-        ) : (
-          <span></span>
-        )}
-      </span>
-
-      <div className="toast-text">
-        <h3 className="toast-text-title">{title}</h3>
-        <p className="toast-text-description">{description}</p>
-      </div>
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ opacity: 0, scale: 0.5 }}
+        className={`${toastClass}`}
+      >
+        <span>
+          {variant === "SUCCESS" ? (
+            <Success />
+          ) : variant === "ERROR" ? (
+            <Error />
+          ) : variant === "WARNING" ? (
+            <Warning />
+          ) : variant === "INFO" ? (
+            <Info />
+          ) : null}
+        </span>
+        <div className="toast-text">
+          <h3 className="toast-text-title">{title}</h3>
+          <p className="toast-text-description">{description}</p>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 

6ff6eccac1fee2701cea35190ef2014fe2d9a395
marlon <marlonsuyuc@gmail.com>
2025-05-27 18:24:11 -0600

    (library) refactor: add generateId utility function for unique ID generation

--- toast/src/utils/index.ts
+++ toast/src/utils/index.ts
@@ -1,3 +1,8 @@
 export const cn = (...classes: (string | undefined)[]) => {
   return classes.filter(Boolean).join(" ");
 };
+
+export const generateId = () => {
+  const id = Math.random().toString(36).substring(2, 9);
+  return id;
+};

2fab078ae3f559c5a6ce040be8e7d46b6042d9e5
marlon <marlonsuyuc@gmail.com>
2025-05-28 17:55:18 -0600

    (library) refactor: update tsup.config.ts to use index.tsx and enhance build configuration

--- toast/tsup.config.ts
+++ toast/tsup.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
+  entry: ["src/index.tsx"],
   clean: true, // Clean output directory before each build
+  minify: true,
+  format: ["esm", "cjs"],
+  dts: true, // Generate types
   target: "esnext", // Compile target default to node16
   external: ["react"],
   sourcemap: true,
   injectStyle: true, // Inject CSS as style tags to document head
   tsconfig: "tsconfig.json",
+  banner: {
+    js: '"use client";',
+  },
 });
